68f136f3cf3b00bebaf697041171fb41f0506534
juanj <juanjobelmar@gmail.com>
2019-08-19 09:31:53 +0200

    Da error porque encuentra dos beans

---
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicioComplejo.java
@@ -0,0 +1,14 @@
+package com.bolsadeideas.springboot.di.app.models.services;
+
+import org.springframework.stereotype.Component;
+
+@Component("miServicioComplejo")
+public class MiServicioComplejo implements IServicio {
+
+	@Override
+	public String operacion() {
+		
+		return "Ejecutando un servicio algo más complejo";
+	}
+
+}
